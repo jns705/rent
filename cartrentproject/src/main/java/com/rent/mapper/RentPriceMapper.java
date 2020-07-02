a21faefa7590d0dac055cdffc18899ea44281ab4
soldesk <soldesk@DESKTOP-269E4E0>
2020-07-02 15:20:24 +0900

    aa

--- cartrentproject/src/main/java/com/rent/mapper/RentPriceMapper.java
+++ cartrentproject/src/main/java/com/rent/mapper/RentPriceMapper.java
@@ -1,7 +1,10 @@
 package com.rent.mapper;
 
+import org.springframework.stereotype.Repository;
+
 import com.rent.domain.RentPriceVO;
 
+@Repository("com.rent.mapper.RentPriceMapper")
 public interface RentPriceMapper {
 	
 	//렌트 가격들 등록
