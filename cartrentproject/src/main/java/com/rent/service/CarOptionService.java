5f42be05dc5d7471dcef84acd12990c4c3927c1a
soldesk <soldesk@DESKTOP-269E4E0>
2020-07-03 10:32:32 +0900

    carOptionService

--- cartrentproject/src/main/java/com/rent/service/CarOptionService.java
+++ cartrentproject/src/main/java/com/rent/service/CarOptionService.java
@@ -1,5 +1,18 @@
 package com.rent.service;
 
-public class CarOptionService {
+import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
+import com.rent.mapper.CarOptionMapper;
+
+@Service("com.rent.service.CarOptionService")
+public class CarOptionService {
+	
+	@Resource(name="com.rent.service.CarOptionMapper")
+	CarOptionMapper mapper;
+	
+	
+	
+	
 }
